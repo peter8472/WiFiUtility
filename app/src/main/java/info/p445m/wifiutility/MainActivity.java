2de967340776f816aa3b980972e339eb4c9da39f
peter8472 <peter8472@users.noreply.github.com>
2019-06-05 13:33:16 -0700

    update

--- app/src/main/java/info/p445m/wifiutility/MainActivity.java
+++ app/src/main/java/info/p445m/wifiutility/MainActivity.java
@@ -1,29 +1,63 @@
 package info.p445m.wifiutility;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import info.p445m.wifiutility.dummy.DummyContent;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private Context myself;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myself=this;
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        DummyContent d = new DummyContent();
+        mAdapter = new MyItemRecyclerViewAdapter(d.ITEMS,null);
+
+        recyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "launching other activity", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent newact = new Intent(myself, ViewNetworksActivity.class);
+
+
+                startActivity(newact);
+
             }
         });
     }
---
+++ app/src/main/java/info/p445m/wifiutility/ViewNetworksActivity.java
@@ -0,0 +1,13 @@
+package info.p445m.wifiutility;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewNetworksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_networks);
+    }
+}
